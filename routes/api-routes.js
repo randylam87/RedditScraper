a4bb57df5da6fdcac67521a87c2fddbedbb44377
Ayas87 <randylam87@gmail.com>
2017-07-20 16:14:00 -0700

    added scraping bloomberg articles

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,22 +2,25 @@ const request = require('request');
 const cheerio = require('cheerio');
 
 module.exports = function (app) {
-    app.get('/',(req,res)=>{
-        res.render('home', {});
+    let bloombergurls = [];
+    app.get('/', (req, res) => {
+        let hbsObj = {
+            bloomberg: bloombergurls
+        }
+        res.render('home', hbsObj);
     });
 
-    request('http://reddit.com', (err, res, body)=>{
-        if(!err && res.statusCode == 200){
-            // console.log(err);
-            let $ = cheerio.load('<div id="siteTable"></div>');
-            let urls = [];
-            console.log('------');
-            console.log($);
-            $('a.title').each(()=>{
-                // let url = this.attr('href');
-                // urls.push(url);
+    request('https://www.bloomberg.com/technology', function (err, res, body) {
+        if (!err && res.statusCode == 200) {
+            let $ = cheerio.load(body);
+            $('.story-list-article__content a').each(function (i, element) {
+                let url = $(element).attr('href');
+                let title = $(element).text();
+                bloombergurls.push({
+                    url: url,
+                    title: title
+                });
             });
-            // console.log(urls);
         }
     });
 };
